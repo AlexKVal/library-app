621057cafce19124c5778d8dd45dba77b842a74c
AlexKVal <alexkval@gmail.com>
2015-12-17 15:21:49 +0200

    [added] observer and property

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,5 +1,14 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  isDisabled: true
+  isDisabled: true,
+  emailAddress: '',
+
+  actualEmailAddress: function() {
+    console.log('actualEmailAddress function is called: ', this.get('emailAddress'));
+  }.property('emailAddress'),
+
+  emailAddressChanged: function() {
+    console.log('observer is called', this.get('emailAddress'));
+  }.observes('emailAddress')
 });
