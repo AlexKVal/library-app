2871d1a3b28c31428b01b560ed0932699e5fb7ef
AlexKVal <alexkval@gmail.com>
2015-12-17 15:49:44 +0200

    [changed] use 'computed.empty'

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -3,9 +3,7 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   emailAddress: '',
 
-  isDisabled: Ember.computed('emailAddress', function() {
-    return this.get('emailAddress') === '';
-  }),
+  isDisabled: Ember.computed.empty('emailAddress'),
 
   emailAddressChanged: Ember.observer('emailAddress', function() {
     console.log('observer is called', this.get('emailAddress'));
