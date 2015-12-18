722952233e88a35d7d085ba95d65133c6cc5a3e6
AlexKVal <alexkval@gmail.com>
2015-12-18 19:42:17 +0200

    [added] 'saveInvitation' action

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -3,5 +3,13 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   emailAddress: '',
 
-  isValid: Ember.computed.match('emailAddress', /^.+@.+\..+$/)
+  isValid: Ember.computed.match('emailAddress', /^.+@.+\..+$/),
+
+  actions: {
+    saveInvitation() {
+      alert(`Saving ${this.get('emailAddress')}`);
+      this.set('responseMessage', `Thank you! We've just saved your email address: ${this.get('emailAddress')}`);
+      this.set('emailAddress', '');
+    }
+  }
 });
