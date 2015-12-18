51ffb2cbbb51a3087e88562297a8a5b4e614461e
AlexKVal <alexkval@gmail.com>
2015-12-18 19:35:25 +0200

    [changed] use (siple) email validation for enabling button

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -3,9 +3,5 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   emailAddress: '',
 
-  isDisabled: Ember.computed.empty('emailAddress'),
-
-  emailAddressChanged: Ember.observer('emailAddress', function() {
-    console.log('observer is called', this.get('emailAddress'));
-  })
+  isValid: Ember.computed.match('emailAddress', /^.+@.+\..+$/)
 });
