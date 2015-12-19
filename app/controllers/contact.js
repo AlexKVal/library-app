ffcb302547ab17c9fa229a8c683c2a1289ea6137
AlexKVal <alexkval@gmail.com>
2015-12-19 16:04:43 +0200

    [added] 'sendMessage' action and 'responseMessage'

--- app/controllers/contact.js
+++ app/controllers/contact.js
@@ -6,5 +6,16 @@ export default Ember.Controller.extend({
 
   isEmailValid: Ember.computed.match('email', /^.+@.+\..+$/),
   isMessageValid: Ember.computed.gte('message.length', 5),
-  isSubmitButtonEnabled: Ember.computed.and('isEmailValid', 'isMessageValid')
+  isSubmitButtonEnabled: Ember.computed.and('isEmailValid', 'isMessageValid'),
+
+  responseMessage: '',
+  actions: {
+    sendMessage() {
+      let msg = `Message has been sent to: ${this.get('email')}`;
+      console.log(msg);
+      this.set('responseMessage', msg);
+      this.set('email', '');
+      this.set('message', '');
+    }
+  }
 });
