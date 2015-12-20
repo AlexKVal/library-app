4151c6922ccea7bf029291ae60829b507909f742
AlexKVal <alexkval@gmail.com>
2015-12-20 13:51:30 +0200

    [added] 'invitation' model

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,13 +1,19 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  headerMessage: 'Coming Soon',
+  responseMessage: '',
   emailAddress: '',
 
   isValid: Ember.computed.match('emailAddress', /^.+@.+\..+$/),
 
   actions: {
     saveInvitation() {
-      alert(`Saving ${this.get('emailAddress')}`);
+      let email = this.get('emailAddress');
+
+      let newInvitation = this.store.createRecord('invitation', {email});
+      newInvitation.save();
+
       this.set('responseMessage', `Thank you! We've just saved your email address: ${this.get('emailAddress')}`);
       this.set('emailAddress', '');
     }
