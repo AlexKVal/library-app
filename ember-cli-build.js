e0bc40f513ffacbb678922953d3e53265f018f32
AlexKVal <alexkval@gmail.com>
2015-12-28 01:28:00 +0200

    [added] 'transition.js' to distr

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -4,7 +4,7 @@ var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 module.exports = function(defaults) {
   var app = new EmberApp(defaults, {
     'ember-cli-bootstrap-sassy': {
-      js: ['dropdown']
+      js: ['dropdown', 'transition']
     }
   });
 
